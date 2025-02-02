5553ee459ee9cfc72d154050406a2b3a5a217108
iris1801 <44752281+iris1801@users.noreply.github.com>
2025-02-02 15:26:19 +0100

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,8 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, EqualTo           
-class LoginForm(FlaskForm):                                        username = StringField("Username", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired()])                                                         
-class RegisterForm(FlaskForm):                                     username = StringField("Username", validators=[DataRequired()])                                                               password = PasswordField("Password", validators=[DataRequired()])
+from wtforms.validators import DataRequired, EqualTo
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+
+class RegisterForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
     confirm_password = PasswordField("Conferma password", validators=[DataRequired(), EqualTo('password', message='Le password non corrispondono.')])
     submit = SubmitField("Registrati")
